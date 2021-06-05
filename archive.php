476edb28d08e1e7bd63513dfc4bb9edfec34c235
ryo-1121 <ryo.yamaya@gmail.com>
2021-06-05 23:58:33 +0900

    2021-0605

--- archive.php
+++ archive.php
@@ -1,7 +1,7 @@
 <?php
-/*
-Template Name: ブログアーカイブ
-*/
+// /*
+// Template Name: ブログアーカイブ
+// */
 ?>
 
 
@@ -16,78 +16,36 @@ Template Name: ブログアーカイブ
 <div class="main_container">
 
   <!-------------------------------- blogアーカイブ ----------------------------------->
-
   <div class="blogarchive_list">
 
-  <ul class="blogarchive_list_cont">
-    <li>
-      <label><a href="">
-          <img src="<?php echo get_template_directory_uri() ?>/image/background4.jpg" alt="">
-          <h4 class="blogarchive_list_title">ブログタイトル</h4>
-          <p>2021-05-30</p>
-        </a></label>
-    </li>
-  
-    <li>
-      <label><a href="">
-          <img src="<?php echo get_template_directory_uri() ?>/image/background4.jpg" alt="">
-          <h4 class="blogarchive_list_title">ブログタイトル</h4>
-          <p>2021-05-30</p>
-        </a></label>
-    </li>
-  
-    <li>
-      <label><a href="">
-          <img src="<?php echo get_template_directory_uri() ?>/image/background4.jpg" alt="">
-          <h4 class="blogarchive_list_title">ブログタイトル</h4>
-          <p>2021-05-30</p>
-        </a></label>
-    </li>
-  
-    <li>
-      <label><a href="">
-          <img src="<?php echo get_template_directory_uri() ?>/image/background4.jpg" alt="">
-          <h4 class="blogarchive_list_title">ブログタイトル</h4>
-          <p>2021-05-30</p>
-        </a></label>
-    </li>
-  
-    <li>
-      <label><a href="">
-          <img src="<?php echo get_template_directory_uri() ?>/image/background4.jpg" alt="">
-          <h4 class="blogarchive_list_title">ブログタイトル</h4>
-          <p>2021-05-30</p>
-        </a></label>
-    </li>
-  
-    <li>
-      <label><a href="">
-          <img src="<?php echo get_template_directory_uri() ?>/image/background4.jpg" alt="">
-          <h4 class="blogarchive_list_title">ブログタイトル</h4>
-          <p>2021-05-30</p>
-        </a></label>
-    </li>
-  
-    <li>
-      <label><a href="">
-          <img src="<?php echo get_template_directory_uri() ?>/image/background4.jpg" alt="">
-          <h4 class="blogarchive_list_title">ブログタイトル</h4>
-          <p>2021-05-30</p>
-        </a></label>
-    </li>
-  
-    <li>
-      <label><a href="">
-          <img src="<?php echo get_template_directory_uri() ?>/image/background4.jpg" alt="">
-          <h4 class="blogarchive_list_title">ブログタイトル</h4>
-          <p>2021-05-30</p>
-        </a></label>
-    </li>
-  </ul>
+    <?php
+    $args = array(
+      'numberposts'  => 1000
+    );
+    $my_posts = get_posts($args);
 
+    if (!empty($my_posts)) {
+      $output = '<ul class="blogarchive_list_cont">';
+      foreach ($my_posts as $p) {
+        $output .= '<li class="blogarchive_list_cont_list"><a href="' . get_permalink($p->ID) . '">';
+        $img_id = get_post_thumbnail_id($p);
+        if ($img_id) {
+          $img_URL = wp_get_attachment_image_src($img_id);
+          $output .= '<img src=' . $img_URL[0] . '>';
+        } else {
+          $output .= '<img src=' . get_template_directory_uri() . '/image/Instagram_logo.svg>';
+        }
+        $output .= '<h4 class="blogarchive_list_title">' . $p->post_title . '</h4>';
+        $output .= '<p>' . get_the_date('', $p) . '<br>';
+        $output .= '</p></a></li>';
+      }
+      $output .= '</ul>';
+    }
+    echo $output;
+    ?>
   </div>
-
 </div>
+
 
 <!-------------------------------- jquery ----------------------------------->
 
@@ -104,11 +62,10 @@ Template Name: ブログアーカイブ
     });
   });
 
-  ScrollReveal().reveal('.blogarchive_list_cont', {
+  ScrollReveal().reveal('.blogarchive_list_cont_list', {
     duration: 1500, // アニメーションの完了にかかる時間
     viewFactor: 0.4, // 0~1,どれくらい見えたら実行するか
     reset: true // 何回もアニメーション表示するか
   });
-
 </script>
 <?php get_footer(); ?>
--- page-contactpage.php
+++ page-contactpage.php
@@ -8,95 +8,191 @@ Template Name: コンタクト
 
 <?php get_header(); ?>
 
-    <!-------------------------------- お問い合わせ ----------------------------------->
+<!-------------------------------- お問い合わせ ----------------------------------->
 
-    <button type="button" class="btn" id="c-btn">Open Contact Form</button>
+<div class="main_container">
+    <div class="contactform_main">
 
-    <div id="contact">
-        <form action="#">
-            <ul>
-                <h1>Contact</h1>
-                <li>
-                    <input type="text" name="name" id="name" placeholder="&#xf2c0; Full name">
-                    <input type="email" name="email" id="email" placeholder="&#xf003; Email">
-                </li>
-                <li>
-                    <textarea name="message" id="message" placeholder="&#xf040; Your message"></textarea>
-                </li>
-                <li>
-                    <input type="submit" value="Send message" class="btn" id="submit">
-                </li>
-            </ul>
-        </form>
+    <div id="perspective">
+        <div id="container">
+            <div class="face front">
+                <div id="white">
+                    <h1>Contact Me!<br><span>--------</span></h1>
+                </div>
+            </div>
+            <div class="face back">
+                <div id="open"></div>
+                <div id="folds"></div>
+                <div class="button con">contact form</div>
+                <div id="letter">
+                    <hgroup>
+                        <h1 id="info">TEL:090-3669-4700</h1>
+                        <h2>---- or ----</h2>
+                    </hgroup>
+                    <p>
+
+                    <form action="" method="post" novalidate="novalidate" id="myform">
+
+                        <span class="Name">
+                            <input type="text" name="Name" value="" size="40" class="nameinput" aria-required="true" aria-invalid="false" placeholder="Name">
+                        </span>
+
+                        <span class="Email">
+                            <input type="email" name="Email" size="40" class="emailinput" aria-required="true" aria-invalid="false" placeholder="Email">
+                        </span>
+
+                        <span class="Subject">
+                            <select name="Subject" class="indent" aria-required="true" aria-invalid="false">
+                                <option value="None">Subject</option>
+                                <option value="Job">Job Information</option>
+                                <option value="Booking">Booking</option>
+                            </select>
+                        </span>
+
+                        <span class="Message">
+                            <textarea name="Message" cols="40" rows="10" aria-invalid="false" placeholder="Message"></textarea>
+                        </span>
+
+                        <input type="submit" value="Send" class="button send">
+
+                    </form>
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
+    <div id="wrapper"></div>
+    </div>
+</div>
 
 
-    <!-------------------------------- java script----------------------------------->
-    <script type="text/javascript">
-        (function() {
-            $('html').addClass('js');
+<!-------------------------------- java script----------------------------------->
+<script type="text/javascript">
+    var C = $('#container'),
+        A = $('#open'),
+        L = $('#letter'),
+        B = $('.button.con'),
+        H = $('#letter hgroup h2'),
+        F = $('.front'),
+        W = $('#wrapper'),
+        P = $('#perspective'),
+        closed = true;
+    $(function() {
+        // Handler for .ready() called.
+        $("textarea").text("");
+    });
 
-            var contactForm = {
-                container: $('#contact'),
-                config: {
-                    effect: 'slideToggle',
-                    speed: 200
-                },
-
-                init: function(config) {
-                    $.extend(this.config, config);
-
-                    $('#c-btn').on('click', this.show);
-                },
-
-                show: function() {
-                    var cf = contactForm,
-                        container = cf.container,
-                        config = cf.config;
-
-
-                    if (container.is(':hidden')) {
-                        cf.close.call(container);
-                        container[config.effect]
-                            (config.speed);
-                    }
-                },
-
-                close: function() {
-                    var $this = $('#contact');
-
-                    if ($this.find('span.close').length) return;
-
-                    $('<span class=close>-</span>')
-                        .prependTo(this)
-                        .on('click', function() {
-                            $this[contactForm.config.effect](contactForm.config.speed);
-                        })
-                }
-            };
-
-            contactForm.init({
-                effect: 'fadeToggle',
-                speed: 200
-            });
-        })();
-    </script>
-
-    <!-------------------------------- jquery ----------------------------------->
-
-    <script>
-        $(function() {
-            $('.hamburger').click(function() {
-                $(this).toggleClass('active');
-
-                if ($(this).hasClass('active')) {
-                    $('.globalMenuSp').addClass('active');
-                } else {
-                    $('.globalMenuSp').removeClass('active');
-                }
-            });
+    F.click(function() {
+        C.css({
+            'transition': 'all 1s',
+            'transform': 'rotateY(180deg)',
         });
-    </script>
+        A.css({
+            'transition': 'all 1s .5s',
+            'transform': 'rotateX(180deg)',
+            'z-index': '0'
+        });
+        W.css({
+            'visibility': 'visible'
+        });
+    });
+
+    // open/close and spin
+    W.click(function() {
+        var message = $.trim($('textarea').val());
+        if (message.length > 0) { //they wrote something in the message
+            var r = confirm("You have not sent your message, would you still like to close the form?");
+            if (r == false) //they don't want to close
+            {
+                return;
+            } else //they do want to close. clear message
+            {
+                document.getElementById("myform").reset();
+            }
+        }
+        if (closed === false) {
+            L.css({
+                'transition': 'all .7s',
+                'top': '3px',
+                'height': '200px'
+            });
+            P.css({
+                'transform': 'translateY(0px)'
+            });
+            F.css({
+                'transform': 'rotateZ(0deg)'
+            });
+            H.css({
+                'transition': 'all .5s',
+                'transform': 'rotateZ(0deg)'
+            });
+            C.css({
+                'transition': 'all 1.2s .95s'
+            });
+            A.css({
+                'transition': 'all 1.2s .7s'
+            });
+            H.css({
+                'transition': 'all .5s'
+            });
+            document.getElementById("info").innerHTML = "P:123.456.7890";
+            closed = true;
+        } else {
+            C.css({
+                'transition': 'all 1s .5s',
+            });
+            A.css({
+                'transition': 'all .5s',
+            });
+            closed = false;
+        }
+        C.css({
+            'transform': 'rotateY(0deg) rotate(3deg)'
+        });
+        A.css({
+            'transform': 'rotateX(0deg)',
+            'z-index': '10'
+        });
+        W.css({
+            //'transition':'all .5s',
+            'visibility': 'hidden'
+        });
+    });
+    // Open letter
+    B.click(function() {
+
+        L.css({
+            'transition': 'all .5s 1s',
+            'top': '-600px',
+            'height': '550px'
+        });
+        P.css({
+            'transition': 'all 1s',
+            'transform': 'translateY(450px)'
+        });
+        H.css({
+            'transition': 'all 1s',
+            'transform': 'rotateZ(180deg)'
+        });
+        document.getElementById("info").innerHTML = "Contact Me!";
+    });
+</script>
+
+<!-------------------------------- jquery ----------------------------------->
+
+<script>
+    $(function() {
+        $('.hamburger').click(function() {
+            $(this).toggleClass('active');
+
+            if ($(this).hasClass('active')) {
+                $('.globalMenuSp').addClass('active');
+            } else {
+                $('.globalMenuSp').removeClass('active');
+            }
+        });
+    });
+</script>
 
 
 <?php get_footer(); ?>
